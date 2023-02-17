bc1dfc3aa969b9299783f03c85e10d9171bf9d12
Mbazooka <122548181+Mbazooka@users.noreply.github.com>
2023-02-17 22:37:13 +0100

    bijna correcte versie van monster schieten

--- Level-ADT.rkt
+++ Level-ADT.rkt
@@ -1,16 +1,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                                 Level ADT                                  ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(load "positie-adt.rkt")
-(load "constanten.rkt")
-(load "Hulpprocedures.rkt")
-(load "vectoren-van-posities.rkt")
-(load "Monster-lijst.rkt")
-(load "pad-adt.rkt")
-(load "Projectiel-ADT.rkt")
-(load "Toren-adt.rkt")
-(load "Monster-ADT.rkt")
-
 (define (maak-level-adt pad monster-rij) ;; Neemt een rij van monster in die gereleased zullen zijn op het pad
   (let ((torens '())
         (monsters '()) ;; Lijst omdat elk element bewerken gemakkelijk is
@@ -58,6 +48,9 @@
        (map (lambda (toren)
               (toren 'projectielen))
             torens)))
+
+    ;;Volgende code is om de level te skippen naar het einde
+    ;;Nog toe te voegen
                   
     (define (dispatch msg)
       (cond
@@ -71,42 +64,3 @@
         (else
          "maak-level-adt: ongeldig bericht")))
     dispatch))
-
-;(define pad (maak-pad-adt vector-1))
-;> (define level-1 (maak-level-adt pad voorbeeld-lijst))
-;> (define toren-1 (maak-toren-adt (maak-positie-adt 10 12) 'basis))
-;> ((level-1 'voeg-toren-toe!) toren-1)
-;> (define toren-2 (maak-toren-adt (maak-positie-adt 10 30) 'basis))
-;> ((level-1 'voeg-toren-toe!) toren-2)
-;> (define toren-3 (maak-toren-adt (maak-positie-adt 15 30) 'basis))
-;> ((level-1 'voeg-toren-toe!) toren-3)
-;
-;(define (trace-test proj)
-;  (let ((proj-pos (proj 'positie))
-;        (proj-best (proj 'bestemming)))
-;    (display "Bestemming: ")
-;    (display (proj-best 'x))
-;    (display " ")
-;    (display (proj-best 'y))
-;    (newline)
-;    ((level-1 'update-torens-projectielen!))
-;    ((level-1 'update-torens-projectielen!))
-;    ((level-1 'update-torens-projectielen!))
-;    (display "Huidige positie: ")
-;    (display (proj-pos 'x))
-;    (display " ")
-;    (display (proj-pos 'y))
-;    (newline)))
-;
-;((level-1 'update-monsters!))
-;> ((level-1 'update-monsters!))
-;> ((level-1 'update-monsters!))
-;> ((level-1 'update-monsters!))
-;> ((level-1 'update-monsters!))
-;> ((level-1 'update-monsters!))
-;> ((level-1 'update-monsters!))
-;> ((level-1 'update-torens-projectielen!))
-;> (define test-1 (car ((level-1 'verkrijg-projectielen))))
-;> (define test-2 (car (cddddr ((level-1 'verkrijg-projectielen)))))
-;> (define test-3 (cadr (cddddr ((level-1 'verkrijg-projectielen)))))
-;> (trace-test test-1) (trace-test test-2) (trace-test test-3)
