6023c1f5c7145087d7e4dee9f5e2f9bb171918c6
Mbazooka <122548181+Mbazooka@users.noreply.github.com>
2023-02-20 17:13:18 +0100

    monster lijst aangepastg

--- Monster-lijst.rkt
+++ Monster-lijst.rkt
@@ -1,7 +1,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                              Monster lijst                                 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Volgende code is om een level op te stellen met bepaalde monster die eruit komen
+;; Volgende code is om een level op te stellen met bepaalde monsters die eruit komen
 (define level-1 (list 'rood 'rood 'rood 'rood 'rood
                       'rood 'rood 'rood 'rood 'rood
                       'rood 'rood 'rood 'rood 'rood
